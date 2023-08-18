352fb481c7a30893a6cd56d74fe5f2ccfeada246
Camila <itsmyllaa@github.com>
2023-08-17 21:53:17 -0300

    Typescript project

---
+++ typescript/typescript.ts
@@ -0,0 +1,30 @@
+//Especializar Typescript
+// - O que é
+// - Por que utilizar?
+// - Typescript Playground
+
+// - Verificação tipo estático
+// - Compilação
+// - Tipagem explícita
+// - Any
+// - Interferência de tipos
+// - Tipos primitivos
+// - Matrizes
+// - Funções
+// - Union
+// - Generics
+// - Type
+// - Type Assertions
+// - Objetos
+// - Opcional
+// - Inserção de tipos
+// - Interface
+// - Type e interface
+// - tsconfig
+
+// - Aplicando Typescript
+// - Projeto
+// - Adicionando Typescript 
+// - Tipando componentes
+// - Tipando estados
+// - Tipando resposta da API
